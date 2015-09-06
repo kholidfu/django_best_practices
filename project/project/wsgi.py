eded2a3b5bc4a058b18ab36f88d2548dec77392b
sopier <sopier@gmail.com>
2015-09-06 22:03:03 +0700

    same as before

--- project/project/wsgi.py
+++ project/project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for saham project.
+WSGI config for project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "saham.settings.base")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings.base")
 
 application = get_wsgi_application()
